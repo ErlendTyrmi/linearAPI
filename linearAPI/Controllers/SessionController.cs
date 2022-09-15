31bbd77bc7e8bf39cd4bf2c45c3cea3c0feeaa64
Erlend Tyrmi <erty@tv2.dk>
2022-09-15 16:57:28 +0200

    cookie added

---
+++ linearAPI/Controllers/SessionController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+
+namespace linearAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessionController : ControllerBase
+    {
+        private readonly ILogger<SessionController> _logger;
+
+        public SessionController(ILogger<SessionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        [Produces("application/json")]
+        public IActionResult Login()
+        {
+
+            //// Get credentials
+            //string encodedEmailPassword = authHeader.Substring("Basic ".Length).Trim();
+            //string emailPassword = Encoding
+            //.GetEncoding("iso-8859-1")
+            //.GetString(Convert.FromBase64String(encodedEmailPassword));
+
+            //// Get email and password
+            //int seperatorIndex = emailPassword.IndexOf(':');
+            //string email = emailPassword.Substring(0, seperatorIndex);
+            //string password = emailPassword.Substring(seperatorIndex + 1);
+
+            HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(new ClaimsIdentity("12345"))).Wait();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        [Produces("application/json")]
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+        public IActionResult Logout()
+        {
+            try
+            {
+                HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme).Wait();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
