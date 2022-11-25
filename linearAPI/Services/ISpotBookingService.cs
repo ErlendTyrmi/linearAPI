b2bb8e502dd21d16fe92b522d86eca2c311a6b7e
Erlend Tyrmi <erlend@tyrmi.com>
2022-11-25 14:39:12 +0100

    spotbooking to service (#9)

--- linearAPI/Services/ISpotBookingService.cs
+++ linearAPI/Services/ISpotBookingService.cs
@@ -4,6 +4,8 @@ namespace LinearAPI.Services
 {
     public interface ISpotBookingService
     {
+        SpotBooking? Get(string id);
+        IList<SpotBooking>? GetByUser(LinearUser user);
         int deleteBooking(SpotBooking booking, LinearUser user);
     }
 }
